aa8df81147e87e8f57baa875d21831fb9e8023fc
javier-aguilar <jvr.agx@gmail.com>
2023-10-30 16:27:14 -0600

    Display amount and add buttons

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -1,34 +1,41 @@
 import SquareRoundedIcon from '@mui/icons-material/SquareRounded';
-import { Box, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, Button, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 
 function Nutrition() {   
 
     return (
         <Box sx={{ p: 3}}>
-        <h1>Nutrition</h1>
+        <h1>Calories</h1>
         <List>
             <ListItem>
-                <ListItemText primary="Total Calories: 0" />
+                <ListItemText primary="Total Calories"  secondary="0/1750" />
             </ListItem>
+            <Button variant="contained">Log Calories</Button>
+
+        </List>
+        <h1>Macros</h1>
+        <List>
             <ListItem>
                 <ListItemIcon>
                     <SquareRoundedIcon sx={{color: '#FFF600'}}/>
                 </ListItemIcon>
-                <ListItemText primary="Protein" />
+                <ListItemText primary="Protein" secondary="0 / 175g"/>
             </ListItem>
             <ListItem>
                 <ListItemIcon>
                     <SquareRoundedIcon sx={{color: '#3427E6'}}/>
                 </ListItemIcon>
-                <ListItemText primary="Carbohydrates" />
+                <ListItemText primary="Carbohydrates" secondary="0 / 140g"/>
             </ListItem>
             <ListItem>
                 <ListItemIcon>
                     <SquareRoundedIcon sx={{color: '#9C27E6'}}/>
                 </ListItemIcon>
-                <ListItemText primary="Fat" />
+                <ListItemText primary="Fat" secondary="0 / 54g" />
             </ListItem>
         </List>
+
+        <Button variant="contained">Log Macros</Button>
     </Box>
   )
 }
