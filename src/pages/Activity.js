b0339b457d61a02e2175fd0c80856c2b1ca9350e
javier-aguilar <jvr.agx@gmail.com>
2023-10-30 16:00:41 -0600

    Add Activity page

---
+++ src/pages/Activity.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material';
+
+
+function Activity() {
+  return (
+    <Box sx={{ p: 3}}>
+      <h1>Activity</h1>
+    </Box>
+  )
+}
+
+export default Activity
